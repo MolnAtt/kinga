8a0b0f9b73f38f10f6abb503d5c39c2b5e9167b5
Molnár Attila <molnar.attila@szlgbp.hu>
2024-03-07 10:30:05 +0100

    api

--- app_api/api.py
+++ app_api/api.py
@@ -1,7 +1,8 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework import status
+from random import randint
 
 @api_view(['GET'])
 def randomszam(request):
-    return Response('köszi megkaptam')
+    return Response(randint(1, 20), status=status.HTTP_200_OK)
